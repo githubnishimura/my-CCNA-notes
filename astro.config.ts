a12bad65257a04666449cf2a8b8edffe5a8f5c16
nishimura hironori <36496986+githubnishimura@users.noreply.github.com>
2026-01-23 15:31:12 +0900

    Integrate Cloudflare adapter and change output to server
    Added Cloudflare adapter and updated output settings.

--- astro.config.ts
+++ astro.config.ts
@@ -1,41 +1,40 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import auth from 'auth-astro';
-
-import node from '@astrojs/node';
-import netlify from "@astrojs/netlify";
+import cloudflare from '@astrojs/cloudflare'; // これを追加
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [auth({
-    injectEndpoints: false
-  }), starlight({
-    title: 'MyCCNA Notes',
-    components: {
-      Search: './src/components/starlight/Search.astro'
-    },
-    social: {
-      github: 'https://github.com/TheOtterlord/starlight-auth'
-    },
-    sidebar: [{
-      label: 'Guides',
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: 'Example Guide',
-        link: '/guides/example/'
-      }]
-    }, {
-      label: 'Reference',
-      autogenerate: {
-        directory: 'reference'
-      }
-    }]
-  })],
-  output: 'static',
+  // Cloudflareでログイン機能を使うための設定
+  output: 'server', 
+  adapter: cloudflare(), 
 
-  // Enable the correct adapter for your deployment (note: netlify is untested at the moment)
-  // adapter: node({ mode: 'middleware' })
-  // adapter: netlify()
-  //adapter: vercel()
+  integrations: [
+    auth({
+      injectEndpoints: false
+    }), 
+    starlight({
+      title: 'MyCCNA Notes',
+      components: {
+        Search: './src/components/starlight/Search.astro'
+      },
+      social: {
+        github: 'https://github.com/githubnishimura/my-CCNA-notes' // あなたのURLに直しました
+      },
+      sidebar: [{
+        label: 'Guides',
+        items: [
+          {
+            label: 'Example Guide',
+            link: '/guides/example/'
+          }
+        ]
+      }, {
+        label: 'Reference',
+        autogenerate: {
+          directory: 'reference'
+        }
+      }]
+    })
+  ],
 });
