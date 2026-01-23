911e7c0f45f61d42413d0321928969573b454457
nishimura hironori <36496986+githubnishimura@users.noreply.github.com>
2026-01-23 15:57:24 +0900

    Change output mode to static and update adapter config

--- astro.config.ts
+++ astro.config.ts
@@ -1,44 +1,29 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import auth from 'auth-astro';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare({
-    runtime: { mode: 'complete' },
-    session: {
-      driver: 'kv',
-      binding: 'SESSION' // 先ほどBindingsで設定した変数名
-    }
-  }),
-  // ここを追加：Cloudflareでエラーを起こしやすい画像最適化をバイパスします
+  // テストのため、一旦静的サイト(static)モードにします
+  output: 'static',
+  adapter: cloudflare(),
+  
+  // 画像エラーを避ける設定
   image: {
     service: { entrypoint: 'astro/assets/services/noop' }
   },
+
   integrations: [
-    auth(),
     starlight({
       title: 'MyCCNA Notes',
-      components: {
-        Search: './src/components/starlight/Search.astro'
-      },
       social: {
         github: 'https://github.com/githubnishimura/my-CCNA-notes'
       },
       sidebar: [{
         label: 'Guides',
-        items: [
-          {
-            label: 'Example Guide',
-            link: '/guides/example/'
-          }
-        ]
+        items: [{ label: 'Example Guide', link: '/guides/example/' }]
       }, {
         label: 'Reference',
-        autogenerate: {
-          directory: 'reference'
-        }
+        autogenerate: { directory: 'reference' }
       }]
     })
   ]
